730acbc063720fbf88b2d8d770de88e68f26d5e9
Cory Zue <cory@coryzue.com>
2024-03-14 11:22:44 +0200

    use production redirect url

--- default_settings.py
+++ default_settings.py
@@ -2,6 +2,6 @@
 
 API_BASE = 'https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/STU3/'
 EPIC_APP_ID = '6bedf57b-d6d7-489b-b42e-e91d3dac649a'
-REDIRECT_URI = 'https://302c-152-110-50-122.ngrok-free.app/fhir-app/'
+REDIRECT_URI = 'https://fhir-demo.dimagi.com/fhir-app/'
 PATIENT_ID = 'erXuFYUfucBZaryVksYEcMg3'
 SECRET_KEY = 'BCpuIq9IjKGeq14H0w/pnMvEIJ+AGqQWewSXhDPT0QnMag1yB1M4+dikHSvJhKuJ'
